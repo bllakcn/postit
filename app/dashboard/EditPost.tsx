719aa26c01ae3820d77423411d0869975a221b29
Bilal <52286982+bllakcn@users.noreply.github.com>
2023-03-03 15:58:51 +0100

    Implement deleting posts

--- app/dashboard/EditPost.tsx
+++ app/dashboard/EditPost.tsx
@@ -1,7 +1,11 @@
 "use client";
 
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
 import Image from "next/image";
 import { useState } from "react";
+import toast from "react-hot-toast";
+import Toggle from "./Toggle";
 
 type EditPostProps = {
   id: string;
@@ -22,27 +26,57 @@ export default function EditPost({
   comments,
   id,
 }: EditPostProps) {
+  const [toggle, setToggle] = useState(false);
+  const queryClient = useQueryClient();
+  let deleteToastId: string;
+
+  //Delete post
+  const { mutate } = useMutation({
+    mutationFn: async (id: string) =>
+      await axios.delete("/api/posts/deletePost", { data: id }),
+    onError: (error) => {
+      toast.error("Error deleting that post.", { id: deleteToastId });
+    },
+    onSuccess: (data) => {
+      queryClient.invalidateQueries(["auth-posts"]);
+      toast.success("Post deleted successfully!", { id: deleteToastId });
+    },
+  });
+
+  const deletePost = () => {
+    deleteToastId = toast.loading("Deleting post...", { id: deleteToastId });
+    mutate(id);
+  };
+
   return (
-    <div className="bg-white my-8 p-8 rounded-lg">
-      <div className="flex items-center gap-2">
-        <Image
-          width={32}
-          height={32}
-          src={avatar}
-          alt="avatar"
-          className="rounded-full"
-        />
-        <h3 className="font-bold text-gray-700">{name}</h3>
+    <>
+      <div className="bg-white my-8 p-8 rounded-lg">
+        <div className="flex items-center gap-2">
+          <Image
+            width={32}
+            height={32}
+            src={avatar}
+            alt="avatar"
+            className="rounded-full"
+          />
+          <h3 className="font-bold text-gray-700">{name}</h3>
+        </div>
+        <div className="my-8">
+          <p className="break-all">{title}</p>
+        </div>
+        <div className="flex items-center gap-4">
+          <p className="text-sm font-bold text-gray-700">
+            {comments?.length} Comments
+          </p>
+          <button
+            onClick={(e) => setToggle(true)}
+            className="text-sm font-bold text-red-500"
+          >
+            Delete
+          </button>
+        </div>
       </div>
-      <div className="my-8">
-        <p className="break-all">{title}</p>
-      </div>
-      <div className="flex items-center gap-4">
-        <p className="text-sm font-bold text-gray-700">
-          {comments?.length} Comments
-        </p>
-        <button className="text-sm font-bold text-red-500">Delete</button>
-      </div>
-    </div>
+      {toggle && <Toggle deletePost={deletePost} setToggle={setToggle} />}
+    </>
   );
 }
